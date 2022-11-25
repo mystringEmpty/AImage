a81e21fab8cc59c15bb15dd3b769c69674c25167
string.Empty <mystring.empty@gmail.com>
2022-11-25 16:37:07 +0800

    自用99成新

--- reply/draw.lua
+++ reply/draw.lua
@@ -1,16 +1,92 @@
-msg_reply.draw = {
+msg_reply.nai_draw = {
     keyword = {
-        prefix = {".naifu:"}
+        prefix = {".nai"}
+    },
+    limit = {
+        grp_var = {
+            nai_on = true,
+            nai_ban = false,
+        },
+        user_var = {
+            nai_ban = false,
+        },
+        cd = {
+            user = { aimage = 30 },
+        },
+        today = {
+            user = { aimage = 20 },
+        },
+        lock = "aimage"
     },
     echo = {
-        lua = "draw"
+        lua = "nai.draw"
     }
 }
-msg_reply.delete = {
+msg_reply.nai_off = {
     keyword = {
-        regex = {"(.*)/delete"}
+        prefix = {".nai off"}
     },
-    echo = {
-        lua = "delete"
-    }
+    limit = {
+        grp_id = { nor = {0} },
+        grp_var = { nai_on = true }
+    },
+    echo = function()
+        setGroupConf(msg.gid,"nai_on",false)
+        return "{reply_nai_off}"
+    end
+}
+msg_reply.nai_on = {
+    keyword = {
+        prefix = {".nai on"}
+    },
+    limit = {
+        grp_id = { nor = {0} },
+        grp_var = { nai_on = false }
+    },
+    echo = function()
+        if getUserConf(msg.uid,"trust")>0 or msg.grpAuth>1 then
+            setGroupConf(msg.gid,"nai_on",true)
+            return "{reply_nai_on}"
+        else
+            return "{strPermissionDeniedErr}"
+        end
+    end
+}
+msg_reply.nai_ban = {
+    keyword = {
+        prefix = {".nai ban"}
+    },
+    limit = {
+        user_var = { trust = { at_least = 4 }}
+    },
+    echo = function()
+        local type,id = msg.suffix:match("^[%s]*([ug]?)[^%d]*(%d*)[%s]*$")
+        if type=='g' then
+            if #id==0 then return "{strGroupIDEmpty}" end
+            setGroupConf(id,"nai_ban",true)
+        else
+            if #id==0 then return "{strUIDEmpty}" end
+            setUserConf(id,"nai_ban",true)
+        end
+        return "{reply_nai_ban}"
+    end
+}
+msg_reply.nai_unban = {
+    keyword = {
+        prefix = {".nai unban"}
+    },
+    limit = {
+        user_var = { trust = { at_least = 4 }}
+    },
+    echo = function()
+        local type,id = msg.suffix:match("^[%s]*([ug]?)[^%d]*(%d*)[%s]*$")
+        if type=='g' then
+            if #id==0 then return "{strGroupIDEmpty}" end
+            setGroupConf(id,"nai_ban")
+        else
+            if #id==0 then return "{strUIDEmpty}" end
+            setUserConf(id,"nai_ban")
+        end
+        return "{reply_nai_unban}"
+    end
 }
---
+++ script/nai/draw.lua
@@ -0,0 +1,32 @@
+local hist = getSelfData("aimage/user_hist.json")
+local tags = msg.suffix:match("^[%s]*(.-)[%s]*$")
+if #tags==0 then
+    if not hist[msg.fromUser] then
+        return "{help:aimage}"
+    else
+        tags = hist[msg.fromUser]
+    end
+else
+    hist[msg.fromUser] = tags
+end
+local api = "http://261090.proxy.nscc-gz.cn:8888/"
+local prompt = "masterpiece, best quality, " .. tags
+local frmdata = {
+    prompt = prompt,
+    width = 512,
+    height = 768,
+    cfg_scale = 12,
+    n_iter = 1,
+    steps = 28,
+    seed = -1,
+    batch_size = 1 ,
+    sampler_index = "Euler a" ,
+    negative_prompt = "nsfw, lowres, text, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, sex, missing arms,lowleg, missing legs, extra arms, extra legs"
+}
+msg:echo("{reply_nai_waiting}")
+local status, rcv_data = http.post(api, frmdata)
+if status then
+    return "[CQ:image,url=" .. rcv_data .. "]"
+end
+log(rcv_data,0)
+return "访问网络失败!"..msg:format(rcv_data)
